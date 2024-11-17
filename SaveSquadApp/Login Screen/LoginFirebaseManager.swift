e6ec3a5ea35ebdd41177ae03e9e7daf6b989b093
Colin Kenny <ckenny92@gmail.com>
2024-11-17 14:54:59 -0500

    Added ability to log in

---
+++ SaveSquadApp/Login Screen/LoginFirebaseManager.swift
@@ -0,0 +1,52 @@
+//
+//  LoginFirebaseManager.swift
+//  SaveSquadApp
+//
+//  Created by Colin Kenny on 11/17/24.
+//
+
+import Foundation
+import FirebaseAuth
+
+extension ViewController{
+    
+    @objc func loginButtonTapped(){
+        var email = ""
+        var password = ""
+        if let emailText = loginScreen.textFieldEmail.text,
+           let passwordText = loginScreen.textFieldPassword.text {
+            if !emailText.isEmpty{
+                if !isValidEmail(emailText) {
+                    self.showErrorAlert(message: "Please enter a valid email address.")
+                    return
+                } else {
+                    email = emailText
+                }
+            } else {
+                self.showErrorAlert(message: "Fields cannot be left empty.")
+                return
+            }
+            if !passwordText.isEmpty{
+                if !isValidPassword(passwordText) {
+                    self.showErrorAlert(message: "Incorrect password.")
+                    return
+                } else {
+                    password = passwordText
+                }
+            } else {
+                self.showErrorAlert(message: "Fields cannot be left empty.")
+                return
+            }
+        }
+        //showActivityIndicator()
+        Auth.auth().signIn(withEmail: email, password: password, completion: {(result, error) in
+            if error == nil{
+                let tabBarController = TabBarController()
+                self.navigationController?.pushViewController(tabBarController, animated: true)
+            }else{
+                self.showErrorAlert(message: "Incorrect email or password.")
+            }
+            //self.hideActivityIndicator()
+        })
+    }
+}
--- SaveSquadApp/Login Screen/ViewController.swift
+++ SaveSquadApp/Login Screen/ViewController.swift
@@ -34,11 +34,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         loginScreen.buttonRegister.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
         navigationController?.setNavigationBarHidden(true, animated: true)
+        loginScreen.buttonLogin.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
     }
     
     @objc func registerButtonTapped() {
         let registerViewController = RegisterViewController()
         self.navigationController?.pushViewController(registerViewController, animated: true)
+    }
+    
+    /*
+     Shows appropriate error alert depending on the message value that is inputted.
+     */
+    func showErrorAlert(message:String){
+        let alert = UIAlertController(
+                title: "Error!", message: message,
+                preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
+    }
+    
+    /*
+     Returns boolean value based on whether the inputted string is a valid email address.
+     Code borrowed from https://stackoverflow.com/questions/25471114/how-to-validate-an-e-mail-address-in-swift/25471164#25471164
+     */
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    func isValidPassword(_ password: String) -> Bool {
+        let passwordRegEx = "[A-Z0-9a-z._%+-@$&*!#?]{6,50}"
+        let passwordPred = NSPredicate(format:"SELF MATCHES %@", passwordRegEx)
+        return passwordPred.evaluate(with: password)
     }
 }
 
