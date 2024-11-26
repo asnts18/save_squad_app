ef9a0d4fb92c3236196b0e51468b9de7d4d4487c
Abegail Santos <abegailsantos.1819@gmail.com>
2024-11-25 23:04:27 -0500

    add ExpenseDetailsView and ExpenseDetailsViewController

--- SaveSquadApp/Expense Details Screen/ExpenseDetailsView.swift
+++ SaveSquadApp/Expense Details Screen/ExpenseDetailsView.swift
@@ -2,20 +2,126 @@
 //  ExpenseDetailsView.swift
 //  SaveSquadApp
 //
-//  Created by Colin Kenny on 11/2/24.
+//  Created by Abegail Santos on 11/25/24.
 //
 
 import UIKit
 
 class ExpenseDetailsView: UIView {
-
+    
+    let expenseImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let labelDescription: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let labelAmount: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .darkGray
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let labelCategory: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .darkGray
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let labelDate: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .darkGray
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let buttonEditExpense: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit Expense", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let buttonDeleteExpense: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Delete Expense", for: .normal)
+        button.backgroundColor = .systemRed
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .white
+        setupView()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func setupView() {
+        backgroundColor = .white
+        
+        let infoStackView = UIStackView(arrangedSubviews: [labelAmount, labelCategory, labelDate])
+        infoStackView.axis = .vertical
+        infoStackView.alignment = .center
+        infoStackView.spacing = 10
+        infoStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let buttonStackView = UIStackView(arrangedSubviews: [buttonEditExpense, buttonDeleteExpense])
+        buttonStackView.axis = .vertical
+        buttonStackView.spacing = 10
+        buttonStackView.alignment = .fill
+        buttonStackView.distribution = .fillEqually
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(expenseImageView)
+        addSubview(labelDescription)
+        addSubview(infoStackView)
+        addSubview(buttonStackView)
+        
+        NSLayoutConstraint.activate([
+            expenseImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            expenseImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            expenseImageView.widthAnchor.constraint(equalToConstant: 150),
+            expenseImageView.heightAnchor.constraint(equalToConstant: 150),
+            
+            labelDescription.topAnchor.constraint(equalTo: expenseImageView.bottomAnchor, constant: 20),
+            labelDescription.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            infoStackView.topAnchor.constraint(equalTo: labelDescription.bottomAnchor, constant: 20),
+            infoStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            infoStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            
+            buttonStackView.topAnchor.constraint(equalTo: infoStackView.bottomAnchor, constant: 20),
+            buttonStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            buttonStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            buttonEditExpense.heightAnchor.constraint(equalToConstant: 50),
+            buttonDeleteExpense.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
 }
--- SaveSquadApp/Expense Details Screen/ExpenseDetailsViewController.swift
+++ SaveSquadApp/Expense Details Screen/ExpenseDetailsViewController.swift
@@ -7,23 +7,48 @@
 
 import UIKit
 
+protocol ExpenseDetailDelegate: AnyObject {
+    func editExpense(_ expense: Expense)
+    func deleteExpense(_ expense: Expense)
+}
+
 class ExpenseDetailsViewController: UIViewController {
+
+    let expenseDetailsScreen = ExpenseDetailsView()
+    
+    weak var delegate: ExpenseDetailDelegate?
+    var expense: Expense
+
+    init(expense: Expense) {
+        self.expense = expense
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func loadView() {
+        view = expenseDetailsScreen
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        expenseDetailsScreen.expenseImageView.image = expense.image
+        expenseDetailsScreen.labelDescription.text = expense.description
+        expenseDetailsScreen.labelCategory.text = "Category: \(expense.category ?? "Personal")"
+        expenseDetailsScreen.labelAmount.text = String(format: "Amount: $%.2f", expense.amount ?? 0.00)
+        expenseDetailsScreen.labelDate.text = "Date: \(expense.targetDateFormatted)"
+//
+//        expenseDetailsScreen.buttonEditExpense.addTarget(self, action: #selector(completeGoal), for: .touchUpInside)
+        expenseDetailsScreen.buttonDeleteExpense.addTarget(self, action: #selector(deleteExpense), for: .touchUpInside)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func deleteExpense() {
+        delegate?.deleteExpense(expense)
+        navigationController?.popViewController(animated: true)
     }
-    */
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 
 }
